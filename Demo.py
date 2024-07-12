14b3e16e653b019c40e8fdf8ee8d16bd4fd232fe
Aditya Sahu <adityaonx@gmail.com>
2024-07-12 23:38:53 +0000

    Added More Python

--- Demo.py
+++ Demo.py
@@ -1,9 +1,8 @@
-s = "hello"
-sum = 0
-for i in range(len(s)-1):
-        a= ord(s[i])
-        b= ord(s[i+1])
-        print("i:", i ,"ascii:" ,b)
-        sub = abs(a-b)
-        sum = sum + sub
-# print(sum)
+from strictly import *
+
+@strictly
+def num(n:int):
+        print(n)
+
+
+num("Hello")
